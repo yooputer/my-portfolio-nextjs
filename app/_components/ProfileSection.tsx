616905cbf6107481f476087cd31e61e8f35bcf40
yooputer <yooputer@gmail.com>
2025-06-10 21:54:52 +0900

    fix: [Profile] 소셜 링크 아이템 수정, 툴팁 추가

--- app/_components/ProfileSection.tsx
+++ app/_components/ProfileSection.tsx
@@ -2,24 +2,31 @@ import { Code, Github, FolderHeart, Instagram } from 'lucide-react';
 import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { ProfileImage } from '@/components/ProfileImage';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
 const socialLinks = [
   {
-    icon: Code,
-    href: 'https://yooputer-devlog.tistory.com/',
-  },
-  {
     icon: Github,
+    label: '깃허브',
     href: 'https://github.com/yooputer',
   },
   {
-    icon: Instagram,
-    href: 'https://www.instagram.com/u_jinjinzara',
+    icon: Code,
+    label: '개발블로그',
+    href: 'https://yooputer-devlog.tistory.com/',
   },
   {
     icon: FolderHeart,
+    label: '일상블로그',
     href: 'https://blog.naver.com/rladbwls027',
   },
 ];
+
 export default function ProfileSection() {
   return (
     <Card>
@@ -40,11 +47,20 @@ export default function ProfileSection() {
 
           <div className="flex justify-center gap-2">
             {socialLinks.map((item, index) => (
-              <Button key={index} variant="ghost" className="bg-primary/10" size="icon" asChild>
-                <a href={item.href} target="_blank" rel="noopener noreferrer">
-                  <item.icon className="h-4 w-4" />
-                </a>
-              </Button>
+              <TooltipProvider key={index}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button variant="ghost" className="bg-primary/10" size="icon" asChild>
+                      <a href={item.href} target="_blank" rel="noopener noreferrer">
+                        <item.icon className="h-4 w-4" />
+                      </a>
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>{item.label}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
             ))}
           </div>
         </div>
