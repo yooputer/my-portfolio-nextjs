84306dae3b7206e2cdd95df6d5761088b47ce0ee
김유진 <yoojin027@mteletec.com>
2025-06-12 16:06:03 +0900

    refact: [Projects > 상세] PageLayout 사용하도록 수정

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -2,6 +2,8 @@ import { getProjectContentBySlug } from '@/lib/apis/projects';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import remarkGfm from 'remark-gfm';
 import rehypeSanitize from 'rehype-sanitize';
+import PageLayout from "@/app/_components/PageLayout";
+import withSlugs from "rehype-slug";
 
 interface ProjectDetailProps {
     params: Promise<{ slug: string }>;
@@ -13,35 +15,18 @@ export default async function ProjectList({ params }: ProjectDetailProps) {
 
 
     return (
-        <div className="container py-6 md:py-8 lg:py-12">
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-[240px_1fr_240px] md:gap-8">
-                {/* 왼쪽 사이드바 */}
-                <div className="hidden md:block">
-                    <div className="sticky top-16">
-                    </div>
-                </div>
-
-                {/* 메인 컨텐츠 */}
-                <div>
-                    <div className="prose prose-neutral dark:prose-invert prose-headings:scroll-mt-[var(--header-height)] max-w-none">
-                        <MDXRemote
-                        source={markdown}
-                        options={{
-                            mdxOptions: {
+        <PageLayout>
+            <div className="prose prose-neutral dark:prose-invert prose-headings:scroll-mt-[var(--header-height)] max-w-none">
+                <MDXRemote
+                    source={markdown}
+                    options={{
+                        mdxOptions: {
                             remarkPlugins: [remarkGfm],
-                            rehypePlugins: [rehypeSanitize],
-                            },
-                        }}
-                        />
-                    </div>
-                </div>
-
-                {/* 오른쪽 사이드바 */}
-                <div className="hidden md:block">
-                    <div className="top-4 space-y-6">
-                    </div>
-                </div>
+                            rehypePlugins: [withSlugs, rehypeSanitize],
+                        },
+                    }}
+                />
             </div>
-        </div>
+        </PageLayout>
     );
 }
