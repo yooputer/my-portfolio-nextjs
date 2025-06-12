1e33d2c7532aaa743f552f503f7db0e60f74b87d
김유진 <yoojin027@mteletec.com>
2025-06-12 15:29:25 +0900

    fix: [Footer] 내용 수정

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,11 +1,13 @@
 export default function Footer() {
   return (
     <footer className="border-t">
-      <div className="container flex h-14 items-center justify-center">
-        <p className="text-muted-foreground text-sm">
-          Built with Next.js, Tailwind CSS and shadcn/ui
-        </p>
-      </div>
+        <div className="container flex h-14 items-center justify-center">
+            <p className="text-muted-foreground text-sm text-center">
+                Built with Next.js, Tailwind CSS and shadcn/ui, Notion API.
+                <br/>Deployed with Vercel.
+                <br/>Contact Email : yooputer@gmail.com
+            </p>
+        </div>
     </footer>
   );
 }
